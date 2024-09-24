a78084cd429b4bcef4fb62a0740a72df151cd0eb
Bayram Ali ŞEN <110849653+alisen1453@users.noreply.github.com>
2024-09-24 16:02:12 +0300

    Add files via upload

---
+++ Services/Catalog/FreeDomeCatalog.Catalog/ModelValidationFilters.cs
@@ -0,0 +1,29 @@
+﻿using FreeDomeCatalog.Catalog.Models;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using respons1.Response;
+
+namespace FreeDomeCatalog.Catalog.Filters
+{
+    public class ModelValidationFilters : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+
+
+            if (!context.ModelState.IsValid) { 
+            var errorMessages = context.ModelState.Values
+                         .SelectMany(v => v.Errors)
+                         .Select(e => e.ErrorMessage)
+                         .ToList();
+
+
+            var errorResponse = new ApiResponse<Category>(null, errorMessages);
+            errorResponse.Success = false;
+                context.Result = new BadRequestObjectResult(errorResponse);
+            }
+        }
+    }
+}
