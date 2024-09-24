e813d63fc0a272ccb18e585790c1d8f99ed96da0
Bayram Ali ŞEN <110849653+alisen1453@users.noreply.github.com>
2024-09-24 14:08:23 +0300

    Update CategoryModel.cs

--- FreeDemoCatalog.Entities/Entity/Models/CategoryModel.cs
+++ FreeDemoCatalog.Entities/Entity/Models/CategoryModel.cs
@@ -2,7 +2,11 @@
 {
     public class CategoryModel
     {
-        public string? CategoryName { get; set; }
-        public string? CategoryDescription { get; set; }
+         [Required(ErrorMessage = "Kategory boş greçilemez")]
+        [MaxLength(20, ErrorMessage = "En fazla 20 karakter"), MinLength(5, ErrorMessage = "Minimum 5 karakter")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Açıklama boş greçilemez")]
+        [MaxLength(100, ErrorMessage = "En fazla 100 karakter"), MinLength(5, ErrorMessage = "Minimum 5 karakter")]
+        public string? Description { get; set; }
     }
 }
